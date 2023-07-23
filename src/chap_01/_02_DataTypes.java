dedb5aae042fde11ca4627693195abd5d77807b3
LimHyunBin <dlagusqls152@naver.com>
2023-07-23 22:12:18 +0900

    Initial commit

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+public class _02_DataTypes {
+
+    public static void main(String[] args) { // psvm : 메인메서드 단축키 or main
+
+        System.out.println("Heelo World");
+        System.out.println("안녕하세요? ");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+
+        System.out.println(123 + 345);
+
+    }
+
+}
---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,18 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+
+        String busNo = "상암08";
+
+        int time = 3;
+
+        double km = 1.2;
+
+        System.out.println(busNo + "번 버스");
+        System.out.println("약 " + time + "분 후 도착");
+        System.out.println("남은 거리 " + km + "km");
+
+
+    }
+}
