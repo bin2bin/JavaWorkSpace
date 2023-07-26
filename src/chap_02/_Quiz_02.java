493865e1620c151d6352a9061158ff569f1a522d
LimHyunBin <dlagusqls152@naver.com>
2023-07-26 23:57:25 +0900

    Commit

---
+++ src/chap_02/_Quiz_02.java
@@ -0,0 +1,19 @@
+package chap_02;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+
+        int hight = 111;
+
+        String check = (hight > 120) ? "키가 " + hight + "cm 이므로 탑승 가능합니다." : "키가 " + hight + "cm 이므로 탑승 불가능합니다.";
+
+        System.out.println(check);
+
+
+
+
+
+
+
+    }
+}
