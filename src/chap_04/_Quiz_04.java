ee15653e0d9ea6b81c569483ce7665c35e6e9df1
LimHyunBin <dlagusqls152@naver.com>
2023-09-18 00:13:59 +0900

    Commit

--- src/chap_04/_Quiz_04.java
+++ src/chap_04/_Quiz_04.java
@@ -3,5 +3,32 @@ package chap_04;
 public class _Quiz_04 {
     public static void main(String[] args) {
 
+        /** 주차 요금은 시간당 4000원 (일일 최대 요금은 30000원)
+         *  일반 차량 5시간 주차 시 20000원
+         *  경차 5시간 주차 시 10000원
+         *  장애인 차량 10시간 주차 시 15000원
+         *
+         *  실행결과 : 주차 요금은 xx 원입니다.
+         *  **/
+
+        int hour = 10; // 주차 시간
+        boolean isSmallCar = false; // 경차 여부
+        boolean withDisabledPerson = true; // 장애인 차량 여부
+        
+        int fee = hour * 4000; // 주차 정산 요금 (시간당 4000원 곱하기)
+
+        // 30000원 초과 시 일일 최대 요금으로 수정
+        if (fee > 30000) {
+            fee = 30000; // 일일 최대 요금 적용
+        }
+
+        // 경차 또는 장애인 차량인 경우 50% 할인
+        if(isSmallCar || withDisabledPerson) {
+            fee /= 2; // 50% 할인 적용
+        }
+        
+        // 실행 결과 출력
+        System.out.println("주차 요금은 " + fee + " 원입니다.");
+        
     }
 }
